01e23ffbee1751a676d17da534d4c4c83808da66
Carlo <cacento@hotmail.it>
2019-01-25 22:00:46 +0100

    gui linked to controller. closes #1 result visualization added to gui. Closes #2

--- SearchMatchesGui.py
+++ SearchMatchesGui.py
@@ -2,44 +2,71 @@ from tkinter import *
 
 from RefereeDataCollector import GameFinder
 
-root = Tk()
-text = ""
 
-def search(event):
-    name = name_entry.get()
-    surname = surname_entry.get()
-    a = GameFinder(name, surname)
+class HoverButton(Button):
+    def __init__(self, master, **kw):
+        Button.__init__(self, master=master, **kw)
+        self.defaultBackground = self["background"]
+        self.bind("<Enter>", self.on_enter)
+        self.bind("<Leave>", self.on_leave)
 
-    result_str = ""
-    for game_number in range(len(a.homeTeams)):
-        result_str += "Referees:" + str(a.referee_terns[game_number]) + "\nHome team:" + a.homeTeams[game_number] + "\nOpponent team:" + str(a.opponentTeams[game_number]) + "\nDate:" + str(a.match_dates[game_number]) + "\nTime:" + str(a.match_times[game_number]) + "\n-----------------------------------------------\n"
+    def on_enter(self, e):
+        self['background'] = self['activebackground']
 
-    global textBox
-
-    textBox.config(state=NORMAL)
-    textBox.insert(END, result_str)
-    textBox.config(state=DISABLED)
+    def on_leave(self, e):
+        self['background'] = self.defaultBackground
 
 
+class SearchView():
+    def __init__(self, master):
+        self.master = master
 
-name_label = Label(root, text="Nome")
-name_label.grid(row=0, sticky=W)
+        self.name_label = Label(root, text="Nome")
+        self.name_label.grid(row=0, sticky=W)
 
-name_entry = Entry(root)
-name_entry.grid(row=0, column=1, sticky=W)
+        self.name_entry = Entry(root)
+        self.name_entry.grid(row=0, column=1, sticky=W)
 
-surname_label = Label(root, text="Cognome")
-surname_label.grid(row=1, sticky=W)
+        self.surname_label = Label(root, text="Cognome")
+        self.surname_label.grid(row=1, sticky=W)
 
-surname_entry = Entry(root)
-surname_entry.grid(row=1, column=1, sticky=W)
+        self.surname_entry = Entry(root)
+        self.surname_entry.grid(row=1, column=1, sticky=W)
+
+        self.findButton = HoverButton(root, text="Search", activebackground='green')
+        self.findButton.grid(row=2, column=1, sticky=E)
+        self.findButton.bind("<Button-1>", self.search)
+        self.master.bind('<Return>', self.search)
 
 
-findButton = Button(root, text="Search")
-findButton.grid(row=2, column=1, sticky=E)
-findButton.bind("<Button-1>", search)
+    def search(self, event):
+        name = self.name_entry.get()
+        surname = self.surname_entry.get()
+        a = GameFinder(name, surname)
 
-textBox = Text(root, height=10, width=60)
-textBox.grid(row=3, column=1, columnspan=2)
+        result_str = ""
+        for game_number in range(len(a.homeTeams)):
+            result_str += "Referees:" + str(a.referee_terns[game_number]) + "\nHome team:" + a.homeTeams[
+                game_number] + "\nOpponent team:" + str(a.opponentTeams[game_number]) + "\nDate:" + str(
+                a.match_dates[game_number]) + "\nTime:" + str(
+                a.match_times[game_number]) + "\n-----------------------------------------------\n"
 
-root. mainloop()
+        self.search_text_box(result_str)
+
+    def search_text_box(self, result_str):
+        self.textBox = Text(self.master, height=10, width=80)
+        self.textBox.grid(row=3, column=1, columnspan=2)
+
+        self.textBox.config(state=NORMAL)
+        self.textBox.insert(END, result_str)
+        self.textBox.config(state=DISABLED)
+
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root.resizable(False, False)
+    root.geometry("800x600")
+    root.title("My Referees Observer")
+    a = SearchView(root)
+    root.mainloop()
